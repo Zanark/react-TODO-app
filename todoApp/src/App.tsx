73dcd475a86f92a535c349d3f602c0c1acefeb48
Debashish Mishra <mishrad@microsoft.com>
2023-09-21 13:59:20 +0530

    ToDo foundation

--- todoApp/src/App.tsx
+++ todoApp/src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className='new-ToDo-item'>
+        <div className='form-row'>
+          <label htmlFor="item">Enter ToDo</label>
+          <input type="text" name="todo" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">ToDo List</h1>
+      <li>
+        <label htmlFor="isCompleted"> Item </label>
+        <input type="checkbox" name="isCompleted" id="isCompleted" />
+      </li>
     </>
   )
 }
